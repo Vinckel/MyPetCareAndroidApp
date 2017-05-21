4ccdf1a68d3f9ed7bb4fe63b5326b40751a8e583
Kyusik Shin <newskys@naver.com>
2017-05-21 18:59:58 +0900

    공지사항 디테일

---
+++ app/src/main/java/petcare/com/mypetcare/Activity/NoticeDetailActivity.java
@@ -0,0 +1,98 @@
+package petcare.com.mypetcare.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import petcare.com.mypetcare.R;
+import petcare.com.mypetcare.Util.GeneralApi;
+
+public class NoticeDetailActivity extends AppCompatActivity {
+    private ImageButton ibBack;
+    private NetworkImageView IvImage;
+    private TextView tvDate;
+    private TextView tvTitle;
+    private TextView tvContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_notice_detail);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionBarView = inflater.inflate(R.layout.actionbar_notice_detail, null);
+
+        ActionBar.LayoutParams lp1 = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(actionBarView, lp1);
+
+        Toolbar parent = (Toolbar) actionBarView.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+
+        ibBack = (ImageButton) findViewById(R.id.ib_notice_detail_back);
+        IvImage = (NetworkImageView) findViewById(R.id.iv_notice_detail_image);
+        tvDate = (TextView) findViewById(R.id.tv_notice_detail_date);
+        tvTitle = (TextView) findViewById(R.id.tv_notice_detail_name);
+        tvContent = (TextView) findViewById(R.id.tv_notice_detail_content);
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        NoticeDetailApi noticeDetailApi = new NoticeDetailApi();
+        String url = "http://220.73.175.100:8080/MPMS/mob/mobile.service";
+        String serviceId = "MPMS_11002";
+
+        Map<String, String> header = new HashMap<>();
+        header.put("url", url);
+        header.put("serviceName", serviceId);
+
+        Map<String, String> body = new HashMap<>();
+//        body.put("PET_AR_ID", id);
+//        noticeDetailApi.execute(header, body);
+    }
+
+    public class NoticeDetailApi extends GeneralApi {
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+//            NoticeListVO noticeListVO = GsonUtil.fromJson(result, NoticeListVO.class);
+//            if (noticeListVO.getResultCode() != 0) {
+//                Toast.makeText(MyPostActivity.this, "정보를 불러오는데 실패했습니다.", Toast.LENGTH_SHORT).show();
+//                finish();
+//                return;
+//            }
+//
+//            noticeObject = noticeListVO.getData().get(0);
+
+//            adapter.addItem(null, null, null, null);
+
+//            for () {
+//
+//            }
+        }
+    }
+}
